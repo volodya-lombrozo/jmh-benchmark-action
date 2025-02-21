a7a5a9b064758a96f8236ae43f3a3c302ef80cfe
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-02-21 11:23:37 +0300

    test: add one more GroovyClass

--- tests/CompareTest.groovy
+++ tests/CompareTest.groovy
@@ -1,11 +1,10 @@
 @Grab('org.spockframework:spock-core:2.4-M5-groovy-4.0')
 @Grab('org.json:json:20250107')
 import spock.lang.Specification
-import org.json.JSONObject
+
 import java.nio.file.Files
-import java.nio.file.Paths
-import java.io.File
 import java.nio.file.Path
+import java.nio.file.Paths
 
 class CompareTest extends Specification {
 
@@ -27,7 +26,11 @@ class CompareTest extends Specification {
         )
 
         when:
-        new GroovyShell().evaluate(new File("compare.groovy"))
+        BenchmarkComparator.compareBenchmarks(
+          directory.resolve("base").resolve("benchmark.json").toString(),
+          directory.resolve("pr").resolve("benchmark.json").toString(),
+          actual
+        )
 
         then:
         def output = new String(Files.readAllBytes(Paths.get(actual)))
